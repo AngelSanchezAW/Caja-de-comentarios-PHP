660663af9c3c10c6fc29d716b51c0c6b8d5e0214
Angel Sanchez <angelsanchez314@gmail.com>
2014-08-26 17:19:50 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 	function conectar(){
-		$user="root";
-		$pass="diosteama10";
+		$user="usuario";
+		$pass="contraseña";
 		$server="localhost";
 		$db="comentarios";
 		$con=mysql_connect($server,$user,$pass) or die ("Error al conectar a la base de datos".mysql_error());
